3d3e00183fbc66dc77013268662d4d05342ced5a
TeamLead <mxmgnchrv@gmail.com>
2024-10-25 15:21:56 +0300

    feat: test btn tg

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {TonConnectUIProvider} from "@tonconnect/ui-react";
 
 import "swiper/css/bundle";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
