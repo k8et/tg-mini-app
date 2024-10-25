85176a3ec3b182a410dc5e040fc8079314e7d552
TeamLead <mxmgnchrv@gmail.com>
2024-10-25 15:30:26 +0300

    feat: test btn tg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import { Route, Routes} from "react-router-dom";
 import MainLayout from "./layout";
 import Main from "./pages/Main";
 import Dish from "./pages/Dish";
-
+import {useEffect} from "react";
 
 
 
 function App() {
-
+    useEffect(() => {
+        if (window.Telegram.WebApp) {
+            window.Telegram.WebApp.ready();
+            window.Telegram.WebApp.onEvent('backButtonClicked', () => {
+                console.log("Кнопка 'Назад' нажата");
+            });
+        }
+    }, []);
     return (
         <MainLayout>
             <Routes>
