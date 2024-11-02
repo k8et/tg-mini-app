373a914fe850be330ba12f1c48d5bb34906b0b71
TeamLead <mxmgnchrv@gmail.com>
2024-11-02 13:45:29 +0200

    feat: test

--- src/pages/Friends/index.jsx
+++ src/pages/Friends/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import bg from "../../assets/img/bg-friend.png";
 import FriendItem from "../../components/contents/FriendsItemsContent";
 import CopyButton from "../../components/commons/CopyButton";
@@ -10,15 +10,28 @@ const Friends = () => {
         { year: "2021", username: "@another_user", points: 15 },
     ];
 
-    // Функция для отправки сообщения боту
     const handleInviteFriend = () => {
-        if (window.Telegram.WebApp) {
-            // Проверка, что приложение открыто в Telegram Web App
-            window.Telegram.WebApp.openTelegramLink("@mmxmhmcf_bot");
-        } else {
-            alert("Запустите приложение в Telegram для отправки сообщения.");
+        if (window.Telegram.WebApp && window.Telegram.WebApp.showPopup) {
+            window.Telegram.WebApp.showPopup({
+                title: "Пригласить друга",
+                message: "Пригласите друга и получайте 15% от его дохода!",
+                buttons: [
+                    { id: "share", type: "default", text: "Поделиться с другом" },
+                    { id: "cancel", type: "destructive", text: "Отмена" }
+                ]
+            });
         }
-    };
+    };useEffect(() => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            const webAppVersion = window.Telegram.WebApp.version;
+            console.log("Версия Telegram Web App:", webAppVersion);
+
+            // Вы также можете показать версию на экране
+            alert("Версия Telegram Web App: " + webAppVersion);
+        } else {
+            console.warn("Telegram Web App не найден.");
+        }
+    }, []);
 
     return (
         <div
